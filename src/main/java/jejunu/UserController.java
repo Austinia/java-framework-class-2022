27fb49077b560890485b2549ac42d3ac39cc493b
Austinia <kimdy90919@gmail.com>
2022-05-20 17:03:13 +0900

    restcontroller

--- src/main/java/jejunu/UserController.java
+++ src/main/java/jejunu/UserController.java
@@ -2,10 +2,7 @@ package jejunu;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -58,4 +55,6 @@ public class UserController {
         modelAndView.addObject("e", e);
         return modelAndView;
     }
+
+
 }
